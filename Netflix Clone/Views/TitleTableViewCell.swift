2176cf38180c53872d5d77ada659060c2f6c76a8
Prasi Aravind <prasi040707@gmail.com>
2022-12-03 14:12:08 -0800

    Getting upcoming titles in correct section

---
+++ Netflix Clone/Views/TitleTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TitleTableViewCell.swift
+//  Netflix Clone
+//
+//  Created by Prasi Aravind on 12/3/22.
+//
+
+import UIKit
+
+class TitleTableViewCell: UITableViewCell {
+
+    static let identifier = "TitleTableViewCell"
+
+}
